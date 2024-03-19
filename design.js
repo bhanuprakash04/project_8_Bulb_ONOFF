450c787036eccbf38a61466389655086cc38765c
bhanuprakash04 <144891005+bhanuprakash04@users.noreply.github.com>
2024-03-19 12:51:20 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,7 @@
+function bulbOn()
+{
+    document.getElementById("img").src="https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+}
+function bulbOff(){
+    document.getElementById("img").src="https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+}
